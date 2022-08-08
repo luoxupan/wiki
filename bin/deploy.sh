a186eb3d27763c6c1ec332e5db8764c1c8eadd24
luoxupan <luoxupan@didiglobal.com>
2022-08-08 17:12:54 +0800

    save

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,8 +4,11 @@ git checkout gh-pages
 branch=$(git symbolic-ref --short -q HEAD)
 if [ $branch = "gh-pages" ]
 then
-  rm -rf ../pages/webapp/*
-  mv ../WebApp/dist/* ../pages/webapp/
+  rm -rf ./pages/webapp/*
+  mv ./WebApp/dist/* ./pages/webapp/
+
+  git config --global user.email "ygxqqx@163.com"
+  git config --global user.name "luoxupan"
 
   git add .
   git commit -m"deploy"
@@ -13,5 +16,3 @@ then
 else
   echo 当前分支不是gh-pages
 fi
-
-
