8e650daeb1c635db4e51a229076a43edf110173b
luoxupan <luoxupan@didiglobal.com>
2022-09-13 20:52:32 +0800

    save

--- WebApp/src/components/Options/Options.tsx
+++ WebApp/src/components/Options/Options.tsx
@@ -56,6 +56,7 @@ export function Options() {
                       <Form.Item
                         {...field}
                         className='item-switch'
+                        valuePropName='checked'
                         name={[field.name, 'enabled']}
                       >
                         <Switch />
