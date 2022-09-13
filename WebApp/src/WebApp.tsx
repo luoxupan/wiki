59743750b0bf7fac6dea69b33a8140c8e98c1b47
luoxupan <luoxupan@didiglobal.com>
2022-09-13 19:36:57 +0800

    集成antd

--- WebApp/src/WebApp.tsx
+++ WebApp/src/WebApp.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { UndoRedo } from './components/index';
+import { Button } from 'antd';
 
 export function WebApp() {
   return (
     <div className="web-app">
       <UndoRedo />
+      <Button onClick={() => {
+        console.log('===');
+      }}>
+        Button
+      </Button>
     </div>
   );
 }
--- WebApp/src/index.tsx
+++ WebApp/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { WebApp } from "./WebApp";
+import 'antd/dist/antd.css';
 
 const root = createRoot(
   document.getElementById("root")
