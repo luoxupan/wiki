8ac5e5fc3d823729f90c4b2a60c502d3f57f20aa
luoxupan <luoxupan@didiglobal.com>
2022-09-24 21:40:40 +0800

    save

--- WebApp/build/webpack.base.js
+++ WebApp/build/webpack.base.js
@@ -3,12 +3,17 @@ const webpack = require("webpack");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+/**
+ * npm i -D XXXX --registry https://registry.npmjs.org
+ * npx webpack --help
+ */
+
 module.exports = {
   // target: '', // 需要看看
   // mode: 'production',
   output: {
     // `path` is the folder where Webpack will place your bundles
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     // `publicPath` is where Webpack will load your bundles from (optional)
     // publicPath: '//static.xxxx.com/cdn/',
     filename: '[name]_[hash:8].js',
@@ -16,38 +21,58 @@ module.exports = {
     chunkFilename: 'chunks/[name]_[contenthash:8].js',
     environment: {
       arrowFunction: false, // webpack拼装的代码不要箭头函数
-    }
+    },
+    clean: true, // 清理/dist文件夹
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              // 设置预定义的环境
-              presets:[
-                [
-                  // 指定环境的插件
-                  "@babel/preset-env",
-                  // 配置信息
-                  {
-                    targets: {
-                      "chrome": "135", // 要兼容目标的浏览器
-                      // "ie": "11"
-                    },
-                    "corejs": "3", // 指定corejs的版本
-                    "useBuiltIns": "usage", // 使用corejs的方式 "usage" 表示按需加载
-                  }
-                ]
-              ]
-            }
-          },
-          'ts-loader' // async/await Promise 转换不了 需babel
-        ],
-        exclude: /node_modules/
-      }
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader'],
+      },
+      {
+        test: /\.less$/i,
+        use: ['style-loader','css-loader','less-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+      // {
+      //   test: /\.ts$/,
+      //   use: [
+      //     {
+      //       loader: 'babel-loader',
+      //       options: {
+      //         // 设置预定义的环境
+      //         presets:[
+      //           [
+      //             // 指定环境的插件
+      //             "@babel/preset-env",
+      //             // 配置信息
+      //             {
+      //               targets: {
+      //                 "chrome": "135", // 要兼容目标的浏览器
+      //                 // "ie": "11"
+      //               },
+      //               "corejs": "3", // 指定corejs的版本
+      //               "useBuiltIns": "usage", // 使用corejs的方式 "usage" 表示按需加载
+      //             }
+      //           ]
+      //         ]
+      //       }
+      //     },
+      //     'ts-loader' // async/await Promise 转换不了 需babel
+      //   ],
+      //   exclude: /node_modules/
+      // }
     ]
   },
   optimization: {
@@ -59,15 +84,16 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin({}),
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: 'index.html'
     }),
-    // https://webpack.js.org/plugins/define-plugin/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     new webpack.DefinePlugin({
-      'WEB_ENV': '"production"',
+      'WEB_ENV': '"production"', // 在代码里面直接用WEB_ENV
     }),
   ],
   resolve: {
-    extensions: ['.ts', '.js', '.tsx']
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map', // 研究一下为何这个配置会导致包体积变大
 };
--- WebApp/build/webpack.config.js
+++ WebApp/build/webpack.config.js
@@ -1,7 +1,8 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const base = require('./webpack.base');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
 /**
  * workbox-webpack-plugin文档`service worker`
  * https://developer.chrome.com/docs/workbox/modules/workbox-webpack-plugin/
@@ -10,62 +11,25 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
-const config = {
+const webpack_config =  merge(base, {
   entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: "[name]_[hash:8].js",
-    clean: true, // 清理/dist文件夹
-  },
   devServer: {
     open: true,
     port: 2002,
+    compress: true,
     host: 'localhost',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: 'ts-loader',
-        exclude: ['/node_modules/'],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader','css-loader'],
-      },
-      {
-        test: /\.less$/i,
-        use: ['style-loader','css-loader','less-loader'],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-  },
-};
+});
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = 'production';
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-    }));
+    webpack_config.mode = 'production';
+    // webpack_config.plugins.push(new WorkboxWebpackPlugin.GenerateSW({
+    //   clientsClaim: true,
+    //   skipWaiting: true,
+    // }));
   } else {
-    config.mode = 'development';
+    webpack_config.mode = 'development';
   }
-  return config;
+  return webpack_config;
 };
