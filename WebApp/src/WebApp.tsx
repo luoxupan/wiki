9286644dc0bb999d22c91fa638c0451a1700f510
luoxupan <luoxupan@didiglobal.com>
2022-09-27 22:27:44 +0800

    save

--- WebApp/src/WebApp.tsx
+++ WebApp/src/WebApp.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-// import { UndoRedo } from './components/index';
-import { Options } from './components/index';
+import './WebApp.less';
+import { Outlet, Link } from "react-router-dom";
 
 export function WebApp() {
   return (
     <div className="web-app">
-      {/* <UndoRedo /> */}
-      <Options />
+      <div className="layout-left">
+        <Link to={`undoredo`}>undo redo</Link>
+        <Link to={`options`}>Options</Link>
+        <Link to={`notfound`}>Not Found</Link>
+      </div>
+      <div className="layout-main">
+        <Outlet />
+      </div>
     </div>
   );
 }
