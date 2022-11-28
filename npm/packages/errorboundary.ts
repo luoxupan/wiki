9bcb1e2102bffe53c2f507b4e59e6a7f5ef7b55b
luoxupan <luoxupan@users.noreply.github.com>
2022-11-28 10:15:29 +0000

    deploy: 9d03195c1c695db283e14bc0d5a9b8a5867a7177

--- npm/packages/errorboundary.ts
+++ npm/packages/errorboundary.ts
@@ -1,10 +1,22 @@
-function errorboundary() {
+function errorHandler(data: any) {
+  if (data.type === 'componentdidcatch') {
+  } else if (data.type === 'error') {
+  } else if (data.type === 'unhandledrejection') {
+  } else {
+  }
+  console.log('errordata:::', data);
+}
+
+function errorBoundary() {
+  window.addEventListener('error', (e) => {
+    errorHandler({ type: 'error', data: e });
+  });
+  window.addEventListener('unhandledrejection', (e) => {
+    errorHandler({ type: 'unhandledrejection', data: e });
+  });
   window.addEventListener('message', (e) => {
-    const data = e.data;
-    if (data.type === 'componentDidCatch') {
-      console.log('e:::', e);
-    }
+    errorHandler(e.data);
   });
 }
 
-errorboundary();
+errorBoundary();
