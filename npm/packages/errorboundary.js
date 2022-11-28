b599f678a9920d4a78fb5a5dd4ea3ae730620c1b
luoxupan <luoxupan@users.noreply.github.com>
2022-11-28 11:46:42 +0000

    deploy: 9a792dfc87cbb32867ba3fbc23a2093382b13c93

--- npm/packages/errorboundary.js
+++ npm/packages/errorboundary.js
@@ -1,9 +1,43 @@
 "use strict";
+function loadScript(src) {
+    return new Promise(function (resolve, reject) {
+        if (document.querySelector("script[data-url=\"".concat(src, "\"]"))) {
+            resolve(undefined);
+        }
+        else {
+            var script = document.createElement('script');
+            script.async = true;
+            script.src = src;
+            script.setAttribute('data-url', src);
+            script.onload = resolve;
+            script.onerror = function (error) {
+                reject(new Error('Unable to load ' + src));
+            };
+            document.body.appendChild(script);
+        }
+    });
+}
 var errorTypes = [
     "error" /* ErrorTypes.error */,
     "componentdidcatch" /* ErrorTypes.componentdidcatch */,
     "unhandledrejection" /* ErrorTypes.unhandledrejection */,
 ];
+function screenshot() {
+    var html2canvasurl = 'https://luoxupan.github.io/wiki/npm/lib/html2canvas.min.js';
+    loadScript(html2canvasurl).then(function () {
+        window.html2canvas(document.body).then(function (canvas) {
+            // document.body.appendChild(canvas);
+            var imgData = canvas.toDataURL('image/png');
+            var newWin = window.open('', '_blank');
+            var dataImg = new Image();
+            dataImg.style = "width: 100%";
+            dataImg.src = imgData;
+            newWin.document.write(dataImg.outerHTML);
+            newWin.document.close();
+        });
+    });
+}
+var timer = null;
 function errorHandler(data) {
     if (errorTypes.includes(data.type)) {
         if (data.type === "error" /* ErrorTypes.error */) {
@@ -15,6 +49,8 @@ function errorHandler(data) {
         else {
         }
         console.log('errordata:::', data);
+        clearTimeout(timer);
+        timer = setTimeout(screenshot, 1000);
     }
 }
 function errorBoundary() {
--- npm/packages/errorboundary.ts
+++ npm/packages/errorboundary.ts
@@ -1,3 +1,23 @@
+function loadScript(src: string) {
+  return new Promise(function(resolve, reject) {
+    if (document.querySelector(`script[data-url="${src}"]`)) {
+      resolve(undefined);
+    } else {
+      var script = document.createElement('script');
+      script.async = true;
+      script.src = src;
+      script.setAttribute('data-url', src);
+
+      script.onload = resolve;
+      script.onerror = function(error) {
+        reject(new Error('Unable to load ' + src));
+      };
+
+      document.body.appendChild(script);
+    }
+  });
+}
+
 const enum ErrorTypes {
   error='error',
   componentdidcatch='componentdidcatch',
@@ -10,6 +30,25 @@ const errorTypes = [
   ErrorTypes.unhandledrejection,
 ];
 
+function screenshot() {
+  const html2canvasurl = 'https://luoxupan.github.io/wiki/npm/lib/html2canvas.min.js';
+  loadScript(html2canvasurl).then(() => {
+    (window as any).html2canvas(document.body).then(function(canvas: any) {
+      // document.body.appendChild(canvas);
+      const imgData = canvas.toDataURL('image/png');
+      const newWin: any = window.open('', '_blank');
+
+      const dataImg: any = new Image();
+      dataImg.style = "width: 100%";
+      dataImg.src = imgData;
+
+      newWin.document.write(dataImg.outerHTML);
+      newWin.document.close();
+    });
+  });
+}
+
+let timer: any = null;
 function errorHandler(data: any) {
   if (errorTypes.includes(data.type)) {
     if (data.type === ErrorTypes.error) {
@@ -18,6 +57,8 @@ function errorHandler(data: any) {
     } else {
     }
     console.log('errordata:::', data);
+    clearTimeout(timer);
+    timer = setTimeout(screenshot, 1000);
   }
 }
 
