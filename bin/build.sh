a8288860bcb94b8e209e779d2db0d33cabf80e55
luoxupan <luoxupan@xiaomi.com>
2024-01-14 21:15:59 +0800

    save

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,3 @@
 git checkout gh-pages
 git pull
 git merge master --allow-unrelated-histories
-
-cd ./WebApp
-npm ci
-npm run build:prod
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,6 @@ git checkout gh-pages
 branch=$(git symbolic-ref --short -q HEAD)
 if [ $branch = "gh-pages" ]
 then
-  rm -rf ./pages/webapp/*
-  mv ./WebApp/dist/* ./pages/webapp/
-
   git config --global user.email "ygxqqx@163.com"
   git config --global user.name "luoxupan"
 
