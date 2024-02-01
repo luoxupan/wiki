824c44fd05f21cc252b05f901968541cbb59d265
luoxupan <luoxupan@users.noreply.github.com>
2024-02-01 08:26:30 +0000

    deploy: b89dc51a16d50eca51c2b3ce638e8fa95ee9824b

--- gists/udp/udp_server.c
+++ gists/udp/udp_server.c
@@ -1,30 +1,24 @@
-/*
-Simple udp server
+/**
+ * Simple udp server
 */
-#include<stdio.h>	//printf
-#include<string.h> //memset
-#include<stdlib.h> //exit(0);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
-#define BUFLEN 512	//Max length of buffer
-#define PORT 8888	//The port on which to listen for incoming data
-
-void die(char *s) {
-  perror(s);
-  exit(1);
-}
+#define BUFLEN 512	// Max length of buffer
+#define PORT 8888	// The port on which to listen for incoming data
 
 int main(void) {
   struct sockaddr_in si_me, si_other;
-
-  int s, recv_len;
   socklen_t slen = sizeof(si_other);
   char buf[BUFLEN];
 
-  //create a UDP socket
-  if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-    die("socket");
+  // create a UDP socket
+  int listen_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (listen_socket == -1) {
+    return -1;
   }
 
   // zero out the structure
@@ -34,32 +28,30 @@ int main(void) {
   si_me.sin_port = htons(PORT);
   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  //bind socket to port
-  if (bind(s, (struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
-    die("bind");
+  // bind socket to port
+  if (bind(listen_socket, (struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
+    return -1;
   }
 
-  //keep listening for data
+  // keep listening for data
   while (1) {
     printf("Waiting for data...\n");
     fflush(stdout);
 
-    //try to receive some data, this is a blocking call
-    if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) {
-      die("recvfrom()");
+    // try to receive some data, this is a blocking call
+    int recv_len = recvfrom(listen_socket, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
+    if (recv_len == -1) {
+      exit(1);
     }
 
-    //print details of the client/peer and the data received
     printf("\e[1;34mReceived packet from:\e[0m %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
     printf("\e[1;31mReceived Client Data:\e[0m %s\n" , buf);
-    // printf("\e[1;34m This is a blue text. \e[0m");
 
-    //now reply the client with the same data
-    if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1) {
-      die("sendto()");
+    // now reply the client with the same data
+    if (sendto(listen_socket, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1) {
+      exit(1);
     }
   }
 
-  // close(s);
   return 0;
 }
