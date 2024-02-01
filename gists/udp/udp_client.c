b89dc51a16d50eca51c2b3ce638e8fa95ee9824b
luoxupan <luoxupan@didiglobal.com>
2024-02-01 16:26:09 +0800

    update

--- gists/udp/udp_client.c
+++ gists/udp/udp_client.c
@@ -1,30 +1,25 @@
-/*
-	Simple udp client
+/**
+ * Simple udp client
 */
-#include<stdio.h>	//printf
-#include<string.h> //memset
-#include<stdlib.h> //exit(0);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
 #define SERVER "127.0.0.1"
-#define BUFLEN 512	//Max length of buffer
-#define PORT 8888	//The port on which to send data
-
-void die(char *s) {
-  perror(s);
-  exit(1);
-}
+#define BUFLEN 512	// Max length of buffer
+#define PORT 8888	// The port on which to send data
 
 int main(void) {
   struct sockaddr_in si_other;
-  int s;
   socklen_t slen = sizeof(si_other);
   char buf[BUFLEN];
   char message[BUFLEN];
 
-  if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-    die("socket");
+  int s_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (s_fd == -1) {
+    return -1;
   }
 
   memset((char *) &si_other, 0, sizeof(si_other));
@@ -33,29 +28,28 @@ int main(void) {
 
   if (inet_aton(SERVER , &si_other.sin_addr) == 0) {
     fprintf(stderr, "inet_aton() failed\n");
-    exit(1);
+    return -1;
   }
 
   while (1) {
     printf("Enter message: ");
     fgets(message, BUFLEN, stdin);
 
-    //send the message
-    if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1) {
-      die("sendto()");
+    // send the message
+    if (sendto(s_fd, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1) {
+      exit(1);
     }
 
-    //receive a reply and print it
-    //clear the buffer by filling null, it might have previously received data
+    // receive a reply and print it
+    // clear the buffer by filling null, it might have previously received data
     memset(buf,'\0', BUFLEN);
-    //try to receive some data, this is a blocking call
-    if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) {
-      die("recvfrom()");
+    // try to receive some data, this is a blocking call
+    if (recvfrom(s_fd, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) {
+      exit(1);
     }
 
     printf("\e[1;31mReceived Server Data:\e[0m %s\n" , buf);
   }
 
-  // close(s);
   return 0;
 }
